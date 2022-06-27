b658cd4603b6d73c9e96704acac2e8a725a4e72e
Marian K <marian.kloesler@gmx.de>
2022-06-27 18:40:29 +0200

    update: fixed formatting

--- app/routes/quotes.routes.js
+++ app/routes/quotes.routes.js
@@ -1,19 +1,19 @@
-module.exports = app => {
-    const quotes = require("../controllers/quote.controller.js");
-    var router = require("express").Router();
-    // Create a new Quote
-    router.post("/", quotes.create);
-    // Retrieve all Quotes
-    router.get("/", quotes.findAll);
-    // Retrieve all published Quotes
-    router.get("/authored", quotes.findAllFromAuthor);
-    // Retrieve a single Quote with id
-    router.get("/:id", quotes.findOne);
-    // Update a Quote with id
-    router.put("/:id", quotes.update);
-    // Delete a Quote with id
-    router.delete("/:id", quotes.delete);
-    // Create a new Quote
-    router.delete("/", quotes.deleteAll);
-    app.use('/api/quotes', router);
-  };
+module.exports = (app) => {
+  const quotes = require("../controllers/quote.controller.js");
+  var router = require("express").Router();
+  // Create a new Quote
+  router.post("/", quotes.create);
+  // Retrieve all Quotes
+  router.get("/", quotes.findAll);
+  // Retrieve all published Quotes
+  router.get("/authored", quotes.findAllFromAuthor);
+  // Retrieve a single Quote with id
+  router.get("/:id", quotes.findOne);
+  // Update a Quote with id
+  router.put("/:id", quotes.update);
+  // Delete a Quote with id
+  router.delete("/:id", quotes.delete);
+  // Create a new Quote
+  router.delete("/", quotes.deleteAll);
+  app.use("/api/quotes", router);
+};
